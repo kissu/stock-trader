deff059041fc787d601d8386ff9903ce7bf40373
Konstantin BIFERT <hello@kissu.io>
2018-03-25 23:52:39 +0200

    Wow, Vuex setup (check it again)

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -7,18 +7,13 @@
   import Stock from './Stock.vue'
 
   export default {
-  data() {
-    return {
-      stocks: [
-        { id: 1, name: 'Ferrari', price: 50},
-        { id: 2, name: 'Lamborgini', price: 100},
-        { id: 3, name: 'Audi', price: 150},
-        { id: 4, name: 'Lotus', price: 200},
-      ]
-    }
-  },
   components: {
     appStock: Stock
+  },
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks
+    }
   }
 }
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, name: 'Ferrari', price: 50 },
+  { id: 2, name: 'Lamborgini', price: 100 },
+  { id: 3, name: 'Audi', price: 150 },
+  { id: 4, name: 'Lotus', price: 200 },
+]
