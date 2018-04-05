c6de62da7f9174aa051c87599299cc657aef0705
Konstantin BIFERT <hello@kissu.io>
2018-04-05 23:45:28 +0200

    Add buy & sell mecanic

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,4 +1,21 @@
 <template lang='pug'>
   div
+    app-stock(v-for="stock in stocks" :stock="stock")
 
 </template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import Stock from './Stock.vue'
+
+  export default {
+    computed: {
+      ...mapGetters({
+        stocks: 'stockPortfolio'
+      })
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
