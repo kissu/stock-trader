5202eee9f339b5c7cbaa9137d018f028ae2f4bdb
Konstantin BIFERT <hello@kissu.io>
2018-04-13 00:20:25 +0200

    Add slide animation

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     app-header
     .row
       .col-xs-12
-        router-view
+        transition(name="slide" mode="out-in")
+          router-view
 </template>
 
 <script>
@@ -22,5 +23,33 @@
 <style>
   body {
     padding: 30px;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 200ms ease-out forwards;
+  }
+
+  .slide-leave-active {
+    animation: slide-out 200ms ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transfrom: translateY(-30px);
+      opacity: 0;
+    } to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+      opacity: 1;
+    } to {
+      transform: translateY(-30px);
+      opacity: 0;
+    }
   }
 </style>
